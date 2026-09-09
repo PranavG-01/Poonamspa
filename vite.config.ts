d423dd1aae5f9932c10fbdb3626bd485fb38f911
Pranav Gangumolu <97116866+PranavG-01@users.noreply.github.com>
2026-09-08 21:35:44 -0400

    Change base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Webpage-Created-for-a-Spa-Business/',
+    base: '/',
     
     plugins: [react(), tailwindcss()],
     
